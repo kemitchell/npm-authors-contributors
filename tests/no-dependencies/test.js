4625386e5ec7f970b5e3272a8129b9c4542bbc4a
Kyle E. Mitchell <kyle@kemitchell.com>
2019-02-11 13:49:46 -0800

    Remove licensee test suite

--- tests/no-dependencies/test.js
+++
@@ -1,3 +0,0 @@
-var tap = require('tap')
-
-tap.equal(require('../run')([ ], __dirname).status, 0)
--- tests/whitelisted/test.js
+++
@@ -1,5 +0,0 @@
-var tap = require('tap')
-
-var results = require('../run')([ ], __dirname)
-
-tap.equal(results.status, 0)
